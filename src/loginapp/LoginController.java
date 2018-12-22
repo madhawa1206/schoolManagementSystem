f54051dccfaf5e3f82a4143f359adaa38c9ba25b
madhawa1206 <madhawa1206@gmail.com>
2018-12-23 00:34:38 +0530

    Test DB Connection

--- src/loginapp/LoginController.java
+++ src/loginapp/LoginController.java
@@ -1,4 +1,36 @@
 package loginapp;
 
-public class LoginController {
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+
+    LoginModel loginModel = new LoginModel();
+
+    @FXML
+    private Label dbstatus;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private ComboBox<option> combobox;
+    @FXML
+    private Button loginButton;
+
+    public void initialize(URL url, ResourceBundle rb) {
+        if (this.loginModel.isDatabaseConnected()) {
+           this.dbstatus.setText("Connected to database");
+        } else {
+            this.dbstatus.setText("Not Connected to database");
+        }
+
+        this.combobox.setItems(FXCollections.observableArrayList(option.values()));
+    }
 }
